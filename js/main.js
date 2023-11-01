7d5667314045de6f82d745fda7592a82402ef16a
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-11-01 20:04:55 +0500

    search  towns

--- js/main.js
+++ js/main.js
@@ -17,7 +17,10 @@ listItems.forEach((item) => {
   });
 });
 //БУРГЕР
-//ИНПУТ С КАТЕГОРИЯМИ
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ИНПУТ С КАТЕГОРИЯМИ(всплывающий список)
 let searchArray = []
 document.querySelectorAll('.inputCheck p').forEach((elem) => {
   elem.addEventListener('click', (event) => {
@@ -68,12 +71,63 @@ document.getElementById('multiselect-input').addEventListener('click', function 
   });
   event.stopPropagation();
 });
+//ИНПУТ С КАТЕГОРИЯМИ(всплывающий список)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ФИЛЬТР(всплывающий список)
+let searchTownArray = [];
+
+document.querySelector('.filterBtn').addEventListener('click', () => {
+    document.querySelector('.filterBoxBody').classList.toggle('none');
+});
+document.getElementById('filterBoxChooseTown-input').addEventListener('click', () => {
+    fetch('https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/master/countries.json')
+        .then(res => res.json())
+        .then(json => {
+            render(json.Russia, document.getElementById('filterBoxChooseTown'));
+            document.querySelector('.filterBoxChooseTown').classList.toggle('none');
+            attachEventListenersToTowns();
+        });
+});
+function render(cities, listElement) {
+    listElement.innerHTML = cities.map((city, index) => createTownElemForTownList(city, index)).join('');
+}
+
+function createTownElemForTownList(city, index) {
+    return `
+        <li class="inputCheck liTown" data-index="${index}">
+            <p data-index="${index}">${city}</p><img class="${searchTownArray.includes(city) ? '' : 'none'}" src="./img/Ok.svg" alt="">
+        </li>
+    `;
+}
+function attachEventListenersToTowns() {
+    document.querySelectorAll('.liTown').forEach((elem) => {
+        elem.addEventListener('click', (event) => {
+            const text = elem.querySelector('p').textContent;
+            const img = elem.querySelector('img');
+            searchTownArray.includes(text) ? searchTownArray = searchTownArray.filter(item => item !== text) : searchTownArray.push(text);
+            img.classList.toggle('none', !searchTownArray.includes(text));
+            document.getElementById('filterBoxChooseTown-input').value = searchTownArray.join(', '); // Вывод выбранных городов через запятую
+            console.log(searchTownArray);
+        });
+    });
+}
+document.addEventListener('click', function (event) {
+    const filterBox = document.querySelector('.filterBox');
+    const filterBoxBody = document.querySelector('.filterBoxBody');
+    const filterBoxChooseTown = document.querySelector('.filterBoxChooseTown');
+    if (event.target !== filterBox && !filterBox.contains(event.target)) {
+        filterBoxBody.classList.add('none');
+        filterBoxChooseTown.classList.add('none');
+    }
+});
 
 
 
 
-//ИНПУТ С КАТЕГОРИЯМИ
 
+//ФИЛЬТР(всплывающий список)
 
 
 
